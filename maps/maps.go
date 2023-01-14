37ee6addb007d7abbb7a2d71c0d37df9c3e46897
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2023-01-14 11:08:46 +0700

    feat(maps): add utils to convert maps to struct

---
+++ maps/maps.go
@@ -0,0 +1,17 @@
+package maps
+
+import "encoding/json"
+
+func MapToStruct(data any, v any) error {
+	jsoned, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(jsoned, v)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
