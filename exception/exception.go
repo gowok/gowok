cc626d5af66ed9afd3a6e9375cba5dddb5e7bf4b
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2023-04-01 01:39:59 +0700

    feat(exception): 🚀 add authorization errors

--- exception/exception.go
+++ exception/exception.go
@@ -8,4 +8,12 @@ import (
 var (
 	ConfigNotFound = errors.New("config file not found")
 	ConfigDecoding = func(err error) error { return fmt.Errorf("config decoding failed: %s", err.Error()) }
+
+	EmailAlreadyUsed       = errors.New("email already used")
+	EmailOrPasswordInvalid = errors.New("email or password invalid")
+	InvalidCredentials     = errors.New("invalid credentials")
+
+	TokenGenerationFail = errors.New("token generation fail")
+	InvalidPolicyData   = errors.New("invalid policy data")
+	DeleteRoleWithUsers = errors.New("can't delete role with some users")
 )
