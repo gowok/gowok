0a2590586592a5ce67e38c29640d2ef3cd182cbb
hadihammurabi <hadihammurabi@gmail.com>
2024-12-03 14:16:58 +0700

    feat(exception): not implemented global error

--- exception/exception.go
+++ exception/exception.go
@@ -6,6 +6,7 @@ import (
 )
 
 var (
+	NotImplemented = errors.New("not implemented")
 	ConfigNotFound = errors.New("config file not found")
 	ConfigDecoding = func(err error) error { return fmt.Errorf("config decoding failed: %s", err.Error()) }
 
@@ -17,7 +18,7 @@ var (
 	InvalidPolicyData   = errors.New("invalid policy data")
 	DeleteRoleWithUsers = errors.New("can't delete role with some users")
 
-	ErrNilPointerDeref = errors.New("nil pointer dereference")
-	ErrGetOfNoValue    = errors.New("get of no value")
-	ErrNoDatabaseFound = errors.New("no database found")
+	NilPointerDeref = errors.New("nil pointer dereference")
+	GetOfNoValue    = errors.New("get of no value")
+	NoDatabaseFound = errors.New("no database found")
 )
