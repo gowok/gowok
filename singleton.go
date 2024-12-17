4867d3ae594748c111b2b3dcf913c0e0fdb2eeef
hadihammurabi <hadihammurabi@gmail.com>
2024-12-17 14:13:43 +0700

    fix(singleton): update when get with sending new value

--- singleton.go
+++ singleton.go
@@ -2,10 +2,15 @@ package gowok
 
 type SingletonFunc[T any] func() T
 
-func Singleton[T any](singletonFunc SingletonFunc[T]) func() *T {
+func Singleton[T any](singletonFunc SingletonFunc[T]) func(...T) *T {
 	var value *T
 
-	return func() *T {
+	return func(newValue ...T) *T {
+		if len(newValue) > 0 {
+			value = &newValue[0]
+			return value
+		}
+
 		if value == nil {
 			create := singletonFunc()
 			value = &create
