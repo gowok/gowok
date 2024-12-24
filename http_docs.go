5861f352ceb89349d3cb59934e3505a315cb98be
hadihammurabi <hadihammurabi@gmail.com>
2024-12-24 16:35:34 +0700

    feat(http_docs): implement operation utilities to add definition, param, respond

--- http_docs.go
+++ http_docs.go
@@ -3,6 +3,7 @@ package gowok
 import (
 	"net/http"
 
+	"github.com/go-openapi/jsonreference"
 	"github.com/go-openapi/spec"
 	"github.com/ngamux/ngamux"
 )
@@ -31,14 +32,15 @@ func NewHttpDocs(title, version string) *HttpDocs {
 			Paths: &spec.Paths{
 				Paths: map[string]spec.PathItem{},
 			},
+			Definitions: spec.Definitions{},
 		},
 	}
 
 	return &HttpDocs{&swagger}
 }
 
-func (docs *HttpDocs) New(description string, callback func(*spec.Operation)) func(ngamux.Route) {
-	operation := spec.NewOperation(description)
+func (docs *HttpDocs) New(description string, callback func(*HttpDocsOperation)) func(ngamux.Route) {
+	operation := &HttpDocsOperation{spec.NewOperation(description)}
 	operation.Description = description
 	item := spec.PathItemProps{}
 	return func(route ngamux.Route) {
@@ -52,19 +54,19 @@ func (docs *HttpDocs) New(description string, callback func(*spec.Operation)) fu
 
 		switch route.Method {
 		case http.MethodGet:
-			item.Get = operation
+			item.Get = operation.Operation
 		case http.MethodPost:
-			item.Post = operation
+			item.Post = operation.Operation
 		case http.MethodPut:
-			item.Put = operation
+			item.Put = operation.Operation
 		case http.MethodHead:
-			item.Head = operation
+			item.Head = operation.Operation
 		case http.MethodPatch:
-			item.Patch = operation
+			item.Patch = operation.Operation
 		case http.MethodDelete:
-			item.Delete = operation
+			item.Delete = operation.Operation
 		case http.MethodOptions:
-			item.Options = operation
+			item.Options = operation.Operation
 		}
 		docs.swagger.Paths.Paths[route.Path] = spec.PathItem{
 			PathItemProps: item,
@@ -72,6 +74,81 @@ func (docs *HttpDocs) New(description string, callback func(*spec.Operation)) fu
 	}
 }
 
+type HttpDocsDefinition struct {
+	Name       string
+	Type       string
+	Properties map[string]spec.Schema
+	Example    any
+}
+
+func (docs *HttpDocs) AddDefinition(definput HttpDocsDefinition) spec.Ref {
+	definitions := make(map[string]spec.Schema)
+	for key, def := range docs.swagger.SwaggerProps.Definitions {
+		definitions[key] = def
+	}
+	definition := spec.Schema{
+		SchemaProps: spec.SchemaProps{
+			Type:       []string{definput.Type},
+			Properties: definput.Properties,
+		},
+		SwaggerSchemaProps: spec.SwaggerSchemaProps{},
+	}
+	if definput.Example != nil {
+		definition.SwaggerSchemaProps.Example = definput.Example
+	}
+	definitions[definput.Name] = definition
+	docs.swagger.SwaggerProps.Definitions = spec.Definitions(definitions)
+
+	return spec.Ref{Ref: jsonreference.MustCreateRef("#/definitions/" + definput.Name)}
+}
+
 func (docs HttpDocs) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	ngamux.Res(rw).JSON(docs.swagger)
+}
+
+type HttpDocsOperation struct {
+	*spec.Operation
+}
+
+type HttpDocsParam struct {
+	Name      string
+	In        string
+	Type      []string
+	Required  bool
+	SchemaRef spec.Ref
+}
+
+func (o HttpDocsOperation) AddParam(param HttpDocsParam) {
+	schemaProps := spec.SchemaProps{
+		Nullable: !param.Required,
+		Type:     spec.StringOrArray(param.Type),
+		Ref:      param.SchemaRef,
+	}
+	swaggerSchemaProps := spec.SwaggerSchemaProps{}
+
+	var _param *spec.Parameter
+	switch param.In {
+	case "body":
+		_param = spec.BodyParam(param.Name, &spec.Schema{
+			SchemaProps:        schemaProps,
+			SwaggerSchemaProps: swaggerSchemaProps,
+		})
+	case "path":
+		_param = spec.PathParam(param.Name)
+	}
+	o.Operation.AddParam(_param)
+}
+
+type HttpDocsRespond struct {
+	Produces  []string
+	Responses map[int]spec.ResponseProps
+}
+
+func (o HttpDocsOperation) AddResponds(respond HttpDocsRespond) {
+	o.WithProduces(respond.Produces...)
+	for key, res := range respond.Responses {
+		o.RespondsWith(key, &spec.Response{
+			ResponseProps: res,
+		})
+	}
 }
