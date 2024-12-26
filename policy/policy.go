cf4e6ab65f6dce1f7b56d6b5c11e146d66c47489
hadihammurabi <hadihammurabi@gmail.com>
2024-12-26 16:18:13 +0700

    feat(policy): tested enforcer model for RBAC and ABAC

--- policy/policy.go
+++ policy/policy.go
@@ -2,6 +2,7 @@ package policy
 
 import (
 	"github.com/casbin/casbin/v2"
+	cModel "github.com/casbin/casbin/v2/model"
 )
 
 const (
@@ -22,12 +23,59 @@ type Policy struct {
 	*casbin.Enforcer
 }
 
-func NewPolicy(params ...interface{}) (*Policy, error) {
-	e, err := casbin.NewEnforcer(params...)
+func NewPolicy(model string) (*Policy, error) {
+	m, err := cModel.NewModelFromString(model)
+	if err != nil {
+		return nil, err
+	}
+
+	e, err := casbin.NewEnforcer(m)
 	if err != nil {
 		return nil, err
 	}
 
 	ee := &Policy{e}
 	return ee, nil
+}
+
+func NewPolicyRBAC() (*Policy, error) {
+	model := `
+	[request_definition]
+	r = sub, obj, act
+
+	[policy_definition]
+	p = sub, obj, act
+
+	[role_definition]
+	g = _, _
+
+	[policy_effect]
+	e = some(where (p.eft == allow))
+
+	[matchers]
+  m = (g(r.sub, p.sub) || r.sub == p.sub) && r.obj == p.obj && r.act == p.act
+	`
+
+	return NewPolicy(model)
+}
+
+func NewPolicyABAC() (*Policy, error) {
+	model := `
+  [request_definition]
+  r = sub, obj, act
+
+  [policy_definition]
+  p = sub, obj, act
+
+  [role_definition]
+  g = _, _
+
+  [policy_effect]
+  e = some(where (p.eft == allow))
+
+  [matchers]
+  m = r.sub == p.sub && r.obj == p.obj && r.act == p.act
+	`
+
+	return NewPolicy(model)
 }
