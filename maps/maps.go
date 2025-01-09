a4a500bc4db1b2b28ed55a7c6312b321e1e4a905
hadihammurabi <hadihammurabi@gmail.com>
2025-01-09 15:37:54 +0700

    feat(maps.Get): get data with path, it helps if map is nested

--- maps/maps.go
+++ maps/maps.go
@@ -1,6 +1,9 @@
 package maps
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"strings"
+)
 
 func MapToStruct(data any, v any) error {
 	jsoned, err := json.Marshal(data)
@@ -14,4 +17,36 @@ func MapToStruct(data any, v any) error {
 	}
 
 	return nil
+}
+
+func Get[T any](data map[string]any, path string, defaults ...T) T {
+	var value T
+	if len(defaults) > 0 {
+		value = defaults[0]
+	}
+
+	keys := strings.Split(path, ".")
+	current := data
+
+	for i, key := range keys {
+		val, exists := current[key]
+		if !exists {
+			return value
+		}
+
+		if i == len(keys)-1 {
+			if v, ok := val.(T); ok {
+				return v
+			}
+			return value
+		}
+
+		next, ok := val.(map[string]any)
+		if !ok {
+			return value
+		}
+		current = next
+	}
+
+	return value
 }
