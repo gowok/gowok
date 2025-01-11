357e8c25a4d5c3e538591d1e9ad333a862bef410
hadihammurabi <hadihammurabi@gmail.com>
2025-01-11 20:19:31 +0700

    chore: delete deprecated singleton

--- singleton.go
+++
@@ -1,9 +0,0 @@
-package gowok
-
-import "github.com/gowok/gowok/singleton"
-
-// Singleton creates new object and gets if created.
-// Deprecated: Use singleton.New instead.
-func Singleton[T any](singletonFunc singleton.SingletonFunc[T]) func(...T) *T {
-	return singleton.New(singletonFunc)
-}
