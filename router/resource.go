987c7320198ad7b9eeaaf367a81ebbfbdeb20d0d
hadihammurabi <hadihammurabi@gmail.com>
2025-01-16 02:14:51 +0700

    feat(router): register resource routes

---
+++ router/resource.go
@@ -0,0 +1,33 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/ngamux/ngamux"
+)
+
+type ResourceHandler interface {
+	Index(http.ResponseWriter, *http.Request)
+	Show(http.ResponseWriter, *http.Request)
+	Store(http.ResponseWriter, *http.Request)
+	Update(http.ResponseWriter, *http.Request)
+	Destroy(http.ResponseWriter, *http.Request)
+}
+
+func Resource(path string, resource ResourceHandler, opts ...func(*ngamux.HttpServeMux)) {
+	g := Group(path)
+	for _, opt := range opts {
+		opt(g)
+	}
+	g.Get("", resource.Index)
+	g.Post("", resource.Store)
+	g.Get("/{id}", resource.Show)
+	g.Put("/{id}", resource.Update)
+	g.Delete("/{id}", resource.Destroy)
+}
+
+func WithResourceMiddleware(middlewares ...ngamux.MiddlewareFunc) func(*ngamux.HttpServeMux) {
+	return func(mux *ngamux.HttpServeMux) {
+		mux.Use(middlewares...)
+	}
+}
