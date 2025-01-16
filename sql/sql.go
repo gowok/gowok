960f1175e728698997f4d0058062bc119f8e3cda
hadihammurabi <hadihammurabi@gmail.com>
2025-01-16 10:43:50 +0700

    fix(sql.Configure): async connection making

--- sql/sql.go
+++ sql/sql.go
@@ -5,6 +5,7 @@ import (
 	"log/slog"
 	"strings"
 
+	"github.com/gowok/gowok/async"
 	"github.com/gowok/gowok/config"
 	"github.com/gowok/gowok/health"
 	"github.com/gowok/gowok/some"
@@ -22,46 +23,52 @@ var drivers = map[string][]string{
 
 func Configure(config map[string]config.SQL) {
 	sqls = make(map[string]*sql.DB)
+	tasks := make([]func() (any, error), 0)
 	for name, dbC := range config {
 		if !dbC.Enabled {
 			continue
 		}
+		tasks = append(tasks, func() (any, error) {
+			drivers, ok := drivers[dbC.Driver]
+			if !ok {
+				slog.Warn("unknown", "driver", dbC.Driver, "plugin", plugin)
+				return nil, nil
+			}
 
-		drivers, ok := drivers[dbC.Driver]
-		if !ok {
-			slog.Warn("unknown", "driver", dbC.Driver, "plugin", plugin)
-			continue
-		}
-
-		for _, driver := range drivers {
-			ddb, err := sql.Open(driver, dbC.DSN)
-			if err != nil {
-				if strings.Contains(err.Error(), "unknown driver") {
-					continue
+			for _, driver := range drivers {
+				ddb, err := sql.Open(driver, dbC.DSN)
+				if err != nil {
+					if strings.Contains(err.Error(), "unknown driver") {
+						continue
+					}
+					slog.Warn("failed to connect", "plugin", plugin, "name", name, "error", err)
+					return nil, nil
 				}
-				slog.Warn("failed to connect", "plugin", plugin, "name", name, "error", err)
-				return
+
+				err = ddb.Ping()
+				if err != nil {
+					slog.Warn("failed to connect", "plugin", plugin, "name", name, "error", err)
+					return nil, nil
+				}
+
+				sqls[name] = ddb
+
+				healthName := "sql"
+				if name != "default" {
+					healthName += "-" + name
+				}
+				health.Add(healthName, healthFunc(ddb))
 			}
 
-			err = ddb.Ping()
-			if err != nil {
-				slog.Warn("failed to connect", "plugin", plugin, "name", name, "error", err)
-				return
+			if _, ok := sqls[name]; !ok {
+				slog.Warn("not installed", "driver", dbC.Driver)
 			}
 
-			sqls[name] = ddb
-
-			healthName := "sql"
-			if name != "default" {
-				healthName += "-" + name
-			}
-			health.Add(healthName, healthFunc(ddb))
-		}
-
-		if _, ok := sqls[name]; !ok {
-			slog.Warn("not installed", "driver", dbC.Driver)
-		}
+			return nil, nil
+		})
 	}
+
+	async.All(tasks...)
 }
 
 func DB(name ...string) some.Some[*sql.DB] {
