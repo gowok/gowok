ce8f177250fcf3048bf392b4e3d5f86d88045812
hadihammurabi <hadihammurabi@gmail.com>
2025-01-16 10:42:11 +0700

    feat(async.All): run tasks asynchronously

---
+++ async/all.go
@@ -0,0 +1,35 @@
+package async
+
+import (
+	"sync"
+)
+
+// All runs all tasks
+func All(tasks ...func() (any, error)) ([]any, error) {
+	var wg sync.WaitGroup
+	results := make([]interface{}, 0)
+	var gerr error
+	var gerrOnce sync.Once
+
+	for _, task := range tasks {
+		wg.Add(1)
+		go func(t func() (any, error), eo *sync.Once) {
+			defer wg.Done()
+			data, err := t()
+			if err != nil {
+				gerrOnce.Do(func() {
+					gerr = err
+				})
+			} else {
+				results = append(results, data)
+			}
+		}(task, &gerrOnce)
+	}
+
+	wg.Wait()
+	if gerr != nil {
+		return nil, gerr
+	}
+
+	return results, nil
+}
