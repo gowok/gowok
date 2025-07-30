9f3925936638e30f1d599f9699b1f02c570c5916
hadihammurabi <hadihammurabi@gmail.com>
2025-07-30 19:45:30 +0700

    feat(gowok.DD): print object to screen for debug

--- gowok.go
+++ gowok.go
@@ -1,5 +1,14 @@
 package gowok
 
+import (
+	"encoding/json"
+	"os"
+)
+
 func ToPtr[T any](v T) *T {
 	return &v
+}
+
+func DD(input any) error {
+	return json.NewEncoder(os.Stdout).Encode(input)
 }
