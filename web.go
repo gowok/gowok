67a5c9080e24d21dd11b9b95da7f537df5bbf364
hadihammurabi <hadihammurabi@gmail.com>
2025-09-30 17:40:51 +0700

    fix(Handler): use ctx for easier usage

--- web.go
+++ web.go
@@ -1,8 +1,10 @@
 package gowok
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/gowok/gowok/errors"
 	"github.com/gowok/gowok/web"
@@ -19,13 +21,13 @@ func Server() *http.Server {
 	return web.Server()
 }
 
-func Handler(handler func(res *ngamux.Response, req *ngamux.Request) error) http.HandlerFunc {
+func Handler(handler func(ctx *WebCtx) error) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		res := ngamux.Res(w)
-		err := handler(res, ngamux.Req(r))
+		ctx := &WebCtx{r.Context(), ngamux.Res(w), ngamux.Req(r)}
+		err := handler(ctx)
 		if err != nil {
 			if gowokErr, ok := err.(errors.Error); ok {
-				err = json.NewEncoder(res).Encode(gowokErr)
+				err = json.NewEncoder(ctx.res).Encode(gowokErr)
 				if err == nil {
 					return
 				}
@@ -33,4 +35,96 @@ func Handler(handler func(res *ngamux.Response, req *ngamux.Request) error) http
 			HttpInternalServerError(w, err)
 		}
 	}
+}
+
+type WebCtx struct {
+	ctx context.Context
+	res *ngamux.Response
+	req *ngamux.Request
+}
+
+func (ctx WebCtx) Req() *ngamux.Request {
+	return ctx.req
+}
+
+func (ctx WebCtx) Res() *ngamux.Response {
+	return ctx.res
+}
+
+func (ctx *WebCtx) Write(content []byte) (int, error) {
+	return ctx.res.Write(content)
+}
+
+func (ctx *WebCtx) SetContext(ctxNew context.Context) {
+	ctx.ctx = ctxNew
+}
+
+func (ctx WebCtx) Deadline() (time.Time, bool) {
+	return ctx.ctx.Deadline()
+}
+
+func (ctx WebCtx) Done() <-chan struct{} {
+	return ctx.ctx.Done()
+}
+
+func (ctx WebCtx) Err() error {
+	return ctx.ctx.Err()
+}
+
+func (ctx WebCtx) Value(key any) any {
+	return ctx.ctx.Value(key)
+}
+
+func (ctx *WebCtx) SetValue(key, value any) {
+	ctx.SetContext(context.WithValue(ctx.ctx, key, value))
+}
+
+func (ctx WebCtx) Ok(body ...any) error {
+	HttpOk(ctx.res, body...)
+	return nil
+}
+
+func (ctx WebCtx) HttpBadRequest(body any) error {
+	HttpBadRequest(ctx.res, body)
+	return nil
+}
+
+func (ctx WebCtx) HttpUnauthorized() error {
+	HttpUnauthorized(ctx.res)
+	return nil
+}
+
+func (ctx WebCtx) HttpNotFound() error {
+	HttpNotFound(ctx.res)
+	return nil
+}
+
+func (ctx WebCtx) HttpInternalServerError(body any) error {
+	HttpNotFound(ctx.res)
+	return nil
+}
+
+func (ctx WebCtx) HttpCreated(body any) error {
+	HttpNotFound(ctx.res)
+	return nil
+}
+
+func (ctx WebCtx) HttpForbidden() error {
+	HttpNotFound(ctx.res)
+	return nil
+}
+
+func (ctx WebCtx) HttpConflict(rw http.ResponseWriter, body any) error {
+	HttpNotFound(ctx.res)
+	return nil
+}
+
+func (ctx WebCtx) NoContent(rw http.ResponseWriter) error {
+	HttpNoContent(ctx.res)
+	return nil
+}
+
+func (ctx WebCtx) Accepted(rw http.ResponseWriter, body any) error {
+	HttpAccepted(ctx.res, body)
+	return nil
 }
