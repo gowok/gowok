0aa6fbcb5bf48ecc6a7a6838837436871adc8842
hadihammurabi <hadihammurabi@gmail.com>
2026-01-20 19:29:42 +0700

    feat(json.Configure): set global marshaler and unmarshaler

--- json/json.go
+++ json/json.go
@@ -2,11 +2,20 @@ package json
 
 import "encoding/json"
 
-type marshaler func(any) ([]byte, error)
-type unmarshaler func([]byte, any) error
-
 var globalMarshaler = json.Marshal
 var globalUnmarshaler = json.Unmarshal
+
+func Configure(marshaler func(any) ([]byte, error), unmarshaler func([]byte, any) error) func() {
+	if marshaler != nil {
+		globalMarshaler = marshaler
+	}
+
+	if unmarshaler != nil {
+		globalUnmarshaler = unmarshaler
+	}
+
+	return func() {}
+}
 
 func Marshal(v any) ([]byte, error) {
 	return globalMarshaler(v)
